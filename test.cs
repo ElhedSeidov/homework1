2874bb25dcb97c658fad6ec690adbe489f0bde93
reshadsaidov <alhadss@code.edu.az>
2023-07-31 16:08:13 +0400

    wrote evezlendi to IT brains

--- test.cs
+++ test.cs
@@ -7,7 +7,7 @@ namespace HelloWorld
         static void Main(string[] args)
         {
             Console.WriteLine("Ad daxil et");
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("hello IT brains");
             Console.ReadLine();
         }
     }
